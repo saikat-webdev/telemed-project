d15a20598a8a06fffd07d2bb5e01ba7a6bc8db2b
Ubuntu <ubuntu@ip-172-31-37-176.eu-north-1.compute.internal>
2026-01-16 10:42:35 +0000

    Save manual changes made on live server

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js','resources/js/custom.js'],
+            input: ['resources/css/app.css', 'resources/js/app.js','resources/js/custom.js','resources/css/auth.css','resources/css/dashboard.css'],
             refresh: true,
         }),
         tailwindcss(),
