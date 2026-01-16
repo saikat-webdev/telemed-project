ec3c17bf03ae0dbd919882238ed12712442bef41
Saikat Banerjee <saikat.banerjee@codeclouds.com>
2026-01-16 20:11:04 +0530

    favicon update on login and register page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sign Up | My App</title>
+    <link rel="icon" type="image/png" href="{{ asset('images/favicon.png') }}">
     @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/css/auth.css'])
 </head>
 <body>
